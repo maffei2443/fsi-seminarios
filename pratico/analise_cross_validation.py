cb9aeeb92cbf50a40719240489cabe17b78dead3
munak98 <42423690+munak98@users.noreply.github.com>
2020-01-27 16:02:50 -0300

    Add files via upload

---
+++ pratico/analise_cross_validation.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import collections
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+
+scores = []
+
+#carrega a base com as features e as classes
+data = pd.read_csv("data_classes.csv")
+
+#divide a base em atributos e as classes
+X = data.drop("Class", axis = 1)
+y = data["Class"]
+
+#normaliza os atributos
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+#classificadores que serão testados
+rfc = RandomForestClassifier(n_estimators = 200)
+clf = svm.SVC()
+knn = KNeighborsClassifier(n_neighbors = 5)
+
+#avaliação dos algoritmos usando validação cruzada com 10 folds
+scores.append(cross_val_score(rfc, X, y, cv=10).mean())
+scores.append(cross_val_score(clf, X, y, cv=10).mean())
+scores.append(cross_val_score(knn, X, y, cv=10).mean())
+print(scores)
