fd9bd5ee7de9c4144c920248ec782be9d1d1768d
maffei2443 <leoitu22hotmail.com@gmail.com>
2020-01-28 19:49:55 -0300

    FALTA SOMAR AS MATRIZES DE CONFUSÃO

--- Experimentos_manu/aux.py
+++ Experimentos_manu/aux.py
@@ -7,33 +7,48 @@ import getopt
 import numpy as np
 import pandas as pd
 import pandas
+from collections import Counter
 
-def drop_draw(data: pandas.DataFrame , columns = ['ANN.acc', 'kNN.acc', 'C4.5.acc', 'SVM.acc', 'RF.acc']):
+
+
+ALL_ACC = ['ANN.acc', 'kNN.acc', 'C4.5.acc', 'SVM.acc', 'RF.acc']
+ALL_ACC_SET =  set(ALL_ACC)
+
+def drop_draw_set_class(data: pandas.DataFrame , score_columns = ALL_ACC):
   """
     Recebe um dataframe e dropa linhas cujo valor máximo nas colunas especificadas
     apareça mais de uma vez nelas.
   """
   for i in data.index:
-    maior = np.max( max(data.loc[i, columns]) )
-    for j in columns:
+    maior = np.max( max(data.loc[i, score_columns]) )
+    count = 0
+    for j in score_columns:
         if data.loc[i,j] == maior:
             count += 1
             data.loc[i, "Class"] = j
         if count > 1:
             data.drop(i, axis = 0, inplace=True)
             count = 0
-    count = 0
             break
+  data.drop(score_columns, axis=1, inplace=True)
+  data.reset_index(inplace = True)
   return data
 
-def preprocess_default(path = 'meta-base.csv', columns = None):
+def preprocess_default(data: pandas.DataFrame, score_columns:list = []):
   """
     Pre-procesamento padrao para problemas de classificacao.
     Consistem em:
       - dropar amostras cujo valor maximal da acurácia não é máximo
       - remover colunas com desempenho relativo à auc e f1-measure
   """
-  data = drop_draw(pd.read_csv(path))
   # Remove colunas com medidas auc e f1m
   data.drop(data.filter(regex='(.*?\.auc|.*?\.f1m)').columns, axis=1, inplace=True)
+
+  # Dropar colunas sem vencedor na acuracia
+  data = drop_draw_set_class(data, score_columns) if score_columns else drop_draw_set_class(data)
   return data
+
+
+
+
+
