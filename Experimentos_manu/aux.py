b82cf046485161465d0a2910aa736525fb2deb43
maffei2443 <leoitu22hotmail.com@gmail.com>
2020-01-29 16:45:04 -0300

    pre-processamento para RandomForest x All

--- Experimentos_manu/aux.py
+++ Experimentos_manu/aux.py
@@ -34,6 +34,27 @@ def drop_draw_set_class(data: pandas.DataFrame , score_columns = ALL_ACC):
   data.reset_index(inplace = True)
   return data
 
+def drop_all_vs_rf(data: pandas.DataFrame , score_columns = ALL_ACC):
+  """
+    Recebe um dataframe e dropa linhas cujo valor máximo nas colunas especificadas
+    apareça mais de uma vez nelas.
+  """
+  for i in data.index:
+    maior = np.max( max(data.loc[i, score_columns]) )
+    count = 0
+    for j in score_columns:
+        if data.loc[i,j] == maior:
+            count += 1
+            data.loc[i, "Class"] = j if j.startswith("RF") else "Other"
+        if count > 1:
+            data.drop(i, axis = 0, inplace=True)
+            count = 0
+            break
+  data.drop(score_columns, axis=1, inplace=True)
+  data.reset_index(inplace = True)
+  return data
+
+
 def preprocess_default(data: pandas.DataFrame, score_columns:list = []):
   """
     Pre-procesamento padrao para problemas de classificacao.
