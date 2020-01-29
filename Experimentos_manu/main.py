62104da4e905fcf76aff0cb7395cdc150ffff958
maffei2443 <leoitu22hotmail.com@gmail.com>
2020-01-29 20:30:48 -0300

    add metricas na MAIN; todo: passar para presentation; mostra algumas metricas no Presentation

--- Experimentos_manu/main.py
+++ Experimentos_manu/main.py
@@ -2,6 +2,8 @@ from sklearn.ensemble import RandomForestClassifier
 
 from sklearn.model_selection import cross_val_score, cross_validate
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import precision_recall_fscore_support
 import pandas as pd
 from aux import preprocess_default, ALL_ACC, ALL_ACC_SET
 from itertools import combinations
@@ -14,7 +16,7 @@ if __name__ == '__main__':
   classifiers_list = []
   mat_list = []
   acc_mean_list = []
-  for to_drop in comb_to_drop:
+  for to_drop in comb_to_drop[-1:]:
     data = raw.drop(list(to_drop), axis=1)
     considered_classes = ALL_ACC_SET - set(to_drop)
     data = preprocess_default(data, list( considered_classes ) )
@@ -22,10 +24,9 @@ if __name__ == '__main__':
     X = data.drop('Class', axis=1)
     y = data['Class']
     # 
-    n_splits = 5
+    n_splits = 3
     skf = StratifiedKFold(n_splits=n_splits)
     acc = 0
-    acc_mat = None
     classifiers_list.append([])
     mat_list.append([])
     for train_index, test_index in skf.split(X, y):
@@ -34,20 +35,26 @@ if __name__ == '__main__':
       )
       y_pred = rfc.predict(X.loc[test_index, :], )
       print("Considered classes : ", considered_classes)
-      new_acc = accuracy_score(y_true=y.loc[test_index], y_pred=y_pred)
       new_conf_mat = confusion_matrix(y_true=y.loc[test_index], y_pred=y_pred)
+      new_acc = accuracy_score(y_true=y.loc[test_index], y_pred=y_pred)
       acc += new_acc
-      acc_mat = acc_mat + new_conf_mat if not isinstance(acc_mat, type(None))   else new_conf_mat
-      print(f"Acc: {new_acc}")
-      # print(f"precision: {precision_score(y_true=y.loc[test_index], y_pred=y_pred)}")
-      # print(f"recall: {recall_score(y_true=y.loc[test_index], y_pred=y_pred)}")
-      # print(f"f1: {f1_score(y_true=y.loc[test_index], y_pred=y_pred)}")
+      print(f"accuracy: {new_acc}")
+      print("precision: ", 
+        precision_score(y_true=y.loc[test_index], y_pred=y_pred, average='macro', zero_division=0)
+      )
+      print("recall: ", 
+        recall_score(y_true=y.loc[test_index], y_pred=y_pred, average='macro', zero_division=0)
+      )
+      print("f1_score: ", 
+        f1_score(y_true=y.loc[test_index], y_pred=y_pred, average='macro', zero_division=0)
+      )
+      print("precision_recall_fscore_support : ")
+      print(*precision_recall_fscore_support(y_true=y.loc[test_index], y_pred=y_pred, zero_division=0), sep= '\n')
       mat_list[-1].append( 
-        confusion_matrix(y_true=y.loc[test_index], y_pred=y_pred)
+        new_conf_mat
        )
-      print(f"confusion_matrix:\n {mat_list[-1][-1]}")  
+      # print(f"confusion_matrix:\n{mat_list[-1][-1]}")  
       classifiers_list[-1].append(rfc)      
       print("------------------------")
     print("mean(acc) = ", acc / n_splits)
     acc_mean_list.append(acc / n_splits)
-    print("mean(Matriz de confusao):\n ", acc_mat / n_splits)
